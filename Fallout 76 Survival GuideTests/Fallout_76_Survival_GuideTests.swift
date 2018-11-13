3cda7fc57ec05e270c026e36f1e83710126064d2
Nicole <nicole@nicoles-macbook-pro.local>
2018-11-13 09:59:26 -0800

    Initial Commit

---
+++ Fallout 76 Survival GuideTests/Fallout_76_Survival_GuideTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fallout_76_Survival_GuideTests.swift
+//  Fallout 76 Survival GuideTests
+//
+//  Created by Nicole Makowsky on 11/13/18.
+//  Copyright © 2018 Nicole Makowsky. All rights reserved.
+//
+
+import XCTest
+@testable import Fallout_76_Survival_Guide
+
+class Fallout_76_Survival_GuideTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
