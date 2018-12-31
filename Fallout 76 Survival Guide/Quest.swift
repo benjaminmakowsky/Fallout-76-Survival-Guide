8e67b47583d6628b0400bee96ab07b1a2900f521
Benjamin Makowsky <benjamin.makowsky@gmail.com>
2018-12-31 13:03:47 -0800

    Added quest steps to quest objects

--- Fallout 76 Survival Guide/Quest.swift
+++ Fallout 76 Survival Guide/Quest.swift
@@ -36,15 +36,17 @@ class Quest {
 
 		//----------------Vault 76--------------------------//
 		questList.append(Quest(title: "Reclamation Day", steps: ["Discover Overseer's Mission", "Play or Eject Overseer's holotape", "Leave Vault 76"]))
-		questlist.append(Quest(title: "First Contact"))
+		questlist.append(Quest(title: "First Contact", steps: ["Track Down the Overseer", "Find the Overseer's Camp", "Find the Overseer in Flatwoods", "Register as a Volunteer"]))
 
 		//--------------Responders-------------------------//
-		questlist.append(Quest(title: "Thirst Things First"))
-		questlist.append(Quest(title: "Second Helpings"))
-		questlist.append(Quest(title: "Final Departure"))
-		questlist.append(Quest(title: "An Ounce of Prevention"))
-		questlist.append(Quest(title: "Into the Fire"))
-		questlist.append(Quest(title: "The Missing Link"))
+		questlist.append(Quest(title: "Thirst Things First", steps: ["Locate Kesha McDermott", "Search for Kesha along the nearby river", "Retrieve the Water Testing Kit from Kesha McDermott", "Test a sample of water from the river", "Taste a sample of water from Flatwood's Water Pumps", "Analyze the Water Testing Kit's results in Kesha's lab", 	"Boil water using Dirty Water and Wood fuel", "Check in with the Self-Serve Kiosk]))
+		questlist.append(Quest(title: "Second Helpings", steps: ["Find Delbert Winters in Flatwoods", "Look for Delbert Winters at his home, "Search for training instructions around Delbert's house.", "Cook a Ribeye Steak", "Check in with the Self-Serve Kiosk", "Contact the Responders using the Database"]))
+		questlist.append(Quest(title: "Final Departure", steps: ["Investigate Morgantown Airport", "Learn the fate of the Responders", "Listen to Overseer's Log - Morgantown", "Learn about the Inoculation Project"]))
+		questlist.append(Quest(title: "An Ounce of Prevention", steps: ["Study Doctor Hudson's Research	", "Collect a Type-T Fuse", "Collect a blood sample from a Mole Rat", "Collect a blood sample from a Feral Ghoul", "Collect a blood sample from a wolf", "Load blood samples into the centrifuge", "Install the Type-T Fuse", "Use the sympto-matic to administer the vaccine"]))
+		
+		//TODO: Add side quest objectives
+		questlist.append(Quest(title: "Into the Fire", steps: ["Explore the Charleston Fire Department", "Join the Fire Breathers", "Pass the Knowledge Exam", "Complete Physical Exam", "Initiate the final exam site", "Head to the final exam site", "Collect your kit", "Listen to the Fire Breathers Final Exam Briefing holotape", "Activate the emergency beacon", "Return to Bernie", "Register with the Fire Breather's computer system", "Listen to the priority message"]))
+		questlist.append(Quest(title: "The Missing Link", steps: ["Follow Madigan's Trail", "Complete Rose's Quests to gain Her Trust", "Retrieve the Uplink", "Go to the Free States Bunker"]))
 
 		//----------------Raiders-------------------------//
 		questlist.append(Quest(title: "Signal Strength"))
